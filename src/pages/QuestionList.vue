3f9005ce379b1f932f037750daab19d2969a4fd9
xuzhiwei <jsrdxzw@163.com>
2021-10-21 15:58:58 +0800

    改个名字

--- src/pages/QuestionList.vue
+++ src/pages/QuestionList.vue
@@ -2,6 +2,28 @@
   <q-page padding>
     <q-list>
       <q-item>
+        <q-item-section top avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-badge label="10k" />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset/>
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
         <q-item-section>
           <q-item-label>Single line item</q-item-label>
           <q-item-label caption lines=5>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
@@ -16,6 +38,11 @@
       <q-separator spaced inset/>
 
       <q-item>
+        <q-item-section top avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
         <q-item-section>
           <q-item-label>Single line item</q-item-label>
           <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
